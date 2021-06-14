015e7d1fe1eed20dcb1ed480f79dc93e15dc1739
Dmitry Cherkashin <dg743733@gmail.com>
2021-06-14 20:17:25 +0700

    dima_commit_2

--- database/SQLBuilder/MysqlQueryBuilder.php
+++ database/SQLBuilder/MysqlQueryBuilder.php
@@ -1,15 +1,30 @@
 <?php
+
+//Строитель SQL-запросов
 class MysqlQueryBuilder implements SQLQueryBuilder{
     protected $query;
-    protected function reset(){
+
+    //Очищение предыдущего сгенерированного запроса
+    protected function reset() : void{
         $this->query = new \stdClass();
     }
+
+    //Построение запроса на выборку(1: имя_таблицы, 2: поля из которых нужно получить данные)
     public function select(string $table, array $fields):SQLQueryBuilder{
+        $this->reset();
+        $this->query->base = "SELECT " . implode(", ", $fields) . " FROM " . $table;
+        $this->query->type = 'select';
         return $this;
     }
 
+    //Добавляет
     public function where(string $field, string $value, string $operator= '='):SQLQueryBuilder{
-        $lol = "lol";
+        if(!in_array($this->query->type, ['select', 'update', 'delete']))
+        {
+            throw new \Exception("WHERE может использоваться только в запросах SELECT, UPDATE или DELETE");
+        }
+        $this->query->where[] = "$field $operator '$value'";
+
         return $this;
     }
     public function limit(int $start, int $offset):SQLQueryBuilder{
