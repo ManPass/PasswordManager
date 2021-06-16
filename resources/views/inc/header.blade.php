62e6af79bb657c140fc0847e2073465b6ea2627e
Dmitry Cherkashin <dg743733@gmail.com>
2021-06-16 22:39:29 +0700

    Adding_v_1

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -8,7 +8,7 @@
         <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('home') }}">Home</a>
         <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('about')}}">About</a>
         <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('registraion')}}">Registraion</a>
-        <a class="py-2 text-dark text-decoration-none" href="{{ route('records-data')}}">My Info</a>
-        <a class="py-2 text-dark text-decoration-none" href="{{ route('add')}}">Add data</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('records-data')}}">My Info</a>
+        <a class="me-3 py-2 text-dark text-decoration-none" href="{{ route('add')}}">Add data</a>
       </nav>
     </div>
--- resources/views/myInfo.blade.php
+++ resources/views/myInfo.blade.php
@@ -4,14 +4,23 @@
     Main Page
 @endsection
 
+@section('aside')
+    <form action=""
+@endsection
+
 @section('content')
 <h1>Список паролей:</h1>
     @foreach ( $data as $el )
         <div class="alert alert-info">
-            <h3>{{$el->source}}</h3>
-            <p>{{$el->password}}</p>
-            
-            <a href="#"><button class="btn btn-success">Detail</button></a>
+            <h2>{{$el->source}}</h3>
+            <h3>{{$el->comment}}</h3>
+            @if($el->login != 0)
+                <p>Логин: {{$el->login}}</p>
+            @endif
+            @if($el->url != 0)
+                <p>URL: {{$el->url}}</p>
+            @endif
+            <a href="#"><button class="btn btn-success">Показать пароль</button></a>
         </div>
     @endforeach
 
