793825824e30d5b385334db473f5d38a4226641a
lubuntum <lubuntum@gmail.com>
2021-06-24 12:49:42 +0700

    AdminPanel.blade.php and new @include system for html patterns also fix one problem with Auth, few css styles for containers

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -23,6 +23,7 @@ class adminController extends Controller
             $UserRoles[$user->login] = $rolesEl;
             $count++;
         }
+
         $roles = Role::all();
         return view('admin/admin_page',['data'=> $allUsers, 'UserRoles' => $UserRoles, 'roles' => $roles]);
     }
@@ -31,5 +32,26 @@ class adminController extends Controller
         $role->role= $request->input('role');
         $role->save();
         return redirect()->route('admin_page',['answer' => 'ok']);
+    }
+    public function deleteRole(Request $request){
+        $user = users::where('id',$request['user'])->get();
+
+        $role = Role::where('role',$request['role'])->get()->first()->id;
+        $user_role = UserRole::where('user_id',$request['user'])->where('role_id',$role)->get()->first();
+        $user_role->delete();
+        //$this->showAllUsers($request);
+        //dd($role);
+        return redirect()->route('admin_page',['answer' => 'role_successful_deleted']);
+    }
+    public function addRoleToUser(Request $request){
+
+        $user = users::where('id',$request['user'])->get();
+
+        $user_role = new UserRole();
+        $user_role->user_id = $request['user'];
+        $user_role->role_id = $request['role_id'];
+        $user_role->save();
+
+        return redirect()->route('admin_page',['answer' => 'role_successful_add']);
     }
 }
